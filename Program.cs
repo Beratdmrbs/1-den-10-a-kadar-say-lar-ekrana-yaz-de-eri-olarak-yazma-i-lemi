3367145e160f3739909a626a6f17d5475d513ef6
Beratdmrbs <beratdmrbs61@gmail.com>
2024-11-02 20:45:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿for (int i = 1; i <= 10; i++)
+{
+    string yazı = "";
+
+    switch (i)
+    {
+        case 1:
+            yazı = "BİR";
+            break;
+        case 2:
+            yazı = "İKİ";
+            break;
+        case 3:
+            yazı = "ÜÇ";
+            break;
+        case 4:
+            yazı = "DÖRT";
+            break;
+        case 5:
+            yazı = "BEŞ";
+            break;
+        case 6:
+            yazı = "ALTı";
+            break;
+        case 7:
+            yazı = "YEDİ";
+            break;
+        case 8:
+            yazı = "SEKİZ";
+            break;
+        case 9:
+            yazı = "DOKUZ";
+            break;
+        case 10:
+            yazı = "ON";
+            break;
+        default:
+            yazı = "Geçersiz sayı";
+            break;
+    }
+
+    Console.WriteLine(yazı);
+}
